49837bfd8bfef2c9f8d6eda52808e4a74a593d1b
Rainbow Yang <q1041410345@gmail.com>
2021-05-07 22:37:34 +0800

    添加自动部署脚本

---
+++ deploy.bat
@@ -0,0 +1,9 @@
+call npm run build
+
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f "git@github.com:RainbowYang/Memo.git" master:gh-pages
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import ElementPlus from 'element-plus'
 import 'element-plus/lib/theme-chalk/index.css'
 import App from './App.vue'
 import i18n from './i18n'
+import './registerServiceWorker'
 
 const app = createApp(App).use(i18n)
 app.use(ElementPlus)
